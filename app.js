d5345399247ba70f0ace6cf8f3616a534a9e0788
lvgraham <liavibanez@gmail.com>
2020-07-03 21:32:19 -0500

    adding functions for app.js

--- app.js
+++ app.js
@@ -79,6 +79,84 @@ const moreEmployees = [
     }
 ]
 
+async function Question() {
+    try {
+
+        let mainAnswers = await inquirer.prompt(mainQuestions)
+
+        let role = await nextPrompt(mainAnswers);
+
+        let roleAnswers = await inquirer.prompt(role);
+
+        let empData = await { ...mainAnswers, ...roleAnswers };
+
+        let employee = await makeEmployee(employeeData);
+
+        teamMembers.push(employee);
+
+        let employeeAdd = awayt inquirer.prompt(moreEmployee);
+
+        addMoreOrMakePage(employeeAdd.confirm);
+    }
+    catch (err) {
+        console.log(`uh oh! there's an error somewhere ${err}`)
+    }
+}
+
+function makeEmployee(employee) {
+    let name = employee.name;
+    let id = employee.id;
+    let email = employee.email;
+    let role = employee.role;
+
+    if (role === 'manager'){
+        return new Manager(name, id, email, employee.officeNumber);
+    } else if (role === 'intern'){
+        return new Intern(name, id, email, employee.school);
+    } else if (role === 'engineer'){
+        return new Engineer(name, id, email, emplouyee.github);
+    } else {
+        return 'uh oh! no employee listed';
+    }
+}
+
+
+function nextPrompt(employee){
+    if (employee.role === 'manager'){
+        return mgmtQuestions;
+    } else if (employee.role === 'intern'){
+        return internQuestions;
+    } else if (employee.role === 'engineer'){
+        return engineerQuestions;
+    } else {
+        return 'aw shucks, something went wrong! please choose a role.'
+    }
+}
+
+function addMoreOrMakePage(confirm){
+    if (confirm) {
+        return mainQuestions()
+    }
+
+    fs.access(OUTPUT_DIR, (err) => {
+        if (err) {
+            fs.mkdir(OUTPUT_DIR, (err) => (err) ? console.log(err) : writeHTML());
+        } else (
+            writeHTML()
+        );
+    });
+}
+
+function writeHTML() {
+    let html = render(teamMembers);
+
+    fs.writeFile(outputPath, html, (err) => {
+        (err) ? console.log(err) : console.log('Enjoy your Team page!')
+    });
+}
+
+Question();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
